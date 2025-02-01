c840d72eaf762228b243f660673cf1af2810f907
ogalo <arielgalo39481@gmail.com>
2025-02-01 14:14:18 -0600

    Completing the game.

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 A rock-paper-scissors game against the computer. The code will be in JS.
 
 Already created the functions to obtain the string decisions for both the user and the computer.
+
+The game is now complete and (thanks to ChatGPT) can handle an error on which the user writes a value in the prompt that is not 1,2 or 3.
--- script.js
+++ script.js
@@ -18,31 +18,64 @@ function getChoice(num) {
       break;
     default:
       decision = "Value not allowed";
-      break;
   }
   return decision;
 }
 
 let computerChoice;
+let humanChoice;
+let ties = 0;
+let humanScore = 0;
+let computerScore = 0;
+let winnerMessage;
 let compNumber = () => Math.floor(Math.random() * 3) + 1;
 
 function getComputerChoice() {
   computerChoice = getChoice(compNumber());
-  computerMessage = `Computer chose: ${computerChoice}`;
-  log(computerMessage);
+  log(`Computer chose: ${computerChoice}`);
 }
 
-let humanChoice;
 function getHumanChoice() {
   let promptedChoice = Number(
     window.prompt("Enter 1 for 'Rock', 2 for 'Paper' and 3 for 'Scissors'")
   );
-  let humanChoice = getChoice(promptedChoice);
-  let humanMessage = `Human chose: ${humanChoice}`;
-  log(humanMessage);
+  if (![1, 2, 3].includes(promptedChoice)) {
+    throw new Error("Value not allowed. Game over.");
+  }
+  humanChoice = getChoice(promptedChoice);
+  log(`You chose: ${humanChoice}`);
 }
 
-// for (i=1; i<=2; i++) {
-//   getComputerChoice();
-//   getHumanChoice();
-// }
+function compareResults(cc, hc) {
+  if (cc == hc) {
+    ties++;
+    return (winnerMessage = "This round ends in a tie.");
+  } else if (
+    (cc == "Rock" && hc == "Scissors") ||
+    (cc == "Paper" && hc == "Rock") ||
+    (cc == "Scissors" && hc == "Paper")
+  ) {
+    computerScore++;
+    return (winnerMessage = "You lost this round");
+  } else {
+    humanScore++;
+    return (winnerMessage = "You win this round!");
+  }
+}
+
+function completeGame() {
+  try {
+    log("Welcome to the Rock, Paper, Scissors simulator!");
+    getComputerChoice();
+    getHumanChoice();
+    log(compareResults(computerChoice, humanChoice));
+    log(`The score is: Human = ${humanScore} | Computer = ${computerScore}.`);
+    log("");
+  } catch (error) {
+    log(error.message);
+  }
+}
+
+for (i = 1; i <= 5; i++) {
+  completeGame();
+}
