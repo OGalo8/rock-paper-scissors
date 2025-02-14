aab3c4a820d742b946460ccaac6608b55db1f88a
ogalo8 <arielgalo39481@gmail.com>
2025-02-13 22:57:42 -0600

    Restorating the project in new ubuntu environment

--- README.md
+++ README.md
@@ -9,3 +9,16 @@ The game is now complete and (thanks to ChatGPT) can handle an error on which th
 Changed some of the dynamics of the functions to shape them according to what the initial problem in The Odin Project asked.
 
 Added the overall winner of the game as a console message.
+
+---- \*\*\*\* ----
+Changing the UI. Now we will use buttons to play the game.
+Refactored with the help of ChatGPT. Now almost all is happening inside the getHumanChoice function, because the humanChoice variable only exists inside the function.
+But now, a lot of after-choice logic is taking place inside the playGame function.
+Also, now we can define the max number of rounds per game, and show a winner at the end.
+
+---- \*\*\*\* ----
+At the end of the game, we are now disabling the buttons, so no more interactions can be done (except for reseting the game).
+We now are displaying everything in the UI and not in the console. We added a restart button (with the help of ChatGPT) and it helps us to restart game variables, erase all js-created elements and reactvate button interaction after we obtain an overall winner.
+
+---- \*\*\*\* ----
+Added some styles and currently presenting everything in divs that look like buttons, but I think that maybe we can show the current round, current score and final score as plan text. The restart button should always be visible.
--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-// A program to play rock, paper, scissors against the computer.
+//A script to play a simple rock,paper,scissors game by rounds.
 
-//This shorcut will make our lives easier when printing in
-// the console
-const log = console.log;
+// defining global variables
 
-function getChoice(num) {
+let ties = 0;
+let humanScore = 0;
+let computerScore = 0;
+let gamesPlayed = 1;
+const maxGames = 3;
+
+// will display every result as a <p> in the HTML document.
+
+function displayMessage(message, selector) {
+  let screen = document.querySelector("#" + selector);
+  screen.setAttribute(
+    "style",
+    "font-size: 25px; font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;"
+  );
+  screen.textContent = `${message}`;
+}
+// logic to obtain computerChoice
+function assignComputerAChoice(num) {
   let decision;
   switch (num) {
     case 1:
@@ -22,72 +37,136 @@ function getChoice(num) {
   return decision;
 }
 
-let computerChoice;
-let humanChoice;
-let ties = 0;
-let humanScore = 0;
-let computerScore = 0;
-let winnerMessage;
-
 let compNumber = () => Math.floor(Math.random() * 3) + 1;
 
-function getComputerChoice() {
-  computerChoice = getChoice(compNumber());
-  log(`Computer chose: ${computerChoice}`);
+let getComputerChoice = () => {
+  let cChoice = assignComputerAChoice(compNumber());
+  let computerChoiceMessage = `Computer chose: ${cChoice}`;
+  displayMessage(computerChoiceMessage);
+  return cChoice;
+};
+
+// logic to obtain humanChoice. Everything must initiate here, after the user clicks on a button.
+
+// The control flow will wait until playround receives humanChoice
+function getHumanChoice(playRound) {
+  let choiceButton = document.querySelector("#buttonMenu");
+  function handler(e) {
+    let target = e.target.closest("button"); //this ensures that the event will only start if we click exactly on a button.
+
+    let humanChoice; // if we create humanChoice before user clicks, it will be undefined
+
+    if (!target) return; //if we don't click exactly on a button, nothing will happen
+    let gamesPlayedMessage = `Round ${gamesPlayed} of ${maxGames}`;
+    displayMessage(gamesPlayedMessage, "current-round");
+
+    switch (target.id) {
+      case "rockButton":
+        humanChoice = "rock";
+        break;
+      case "paperButton":
+        humanChoice = "paper";
+        break;
+      case "scissorsButton":
+        humanChoice = "scissors";
+        break;
+    }
+
+    let humanChoiceMessage = `You chose: ${humanChoice}`;
+    displayMessage(humanChoiceMessage);
+
+    playRound(humanChoice);
+
+    if (gamesPlayed > maxGames) {
+      decideWinner();
+      choiceButton.removeEventListener("click", handler);
+    }
+  }
+  choiceButton.addEventListener("click", handler);
 }
 
-function getHumanChoice() {
-  humanChoice = prompt("Write 'rock', 'paper' or 'scissors' ").toLowerCase();
-  if (!["rock", "paper", "scissors"].includes(humanChoice)) {
-    throw new Error("The value you entered is not allowed. Game over.");
-  }
-  log(`You chose: ${humanChoice}`);
+// logic to play a single round, having defined humanChoice
+function playRound(humanChoice) {
+  let computerChoice = getComputerChoice();
+
+  compareResults(computerChoice, humanChoice);
+  let currentScore = `The score is: Human = ${humanScore} | Computer = ${computerScore}. | Ties = ${ties}. `;
+  displayMessage(currentScore);
+
+  gamesPlayed++;
 }
+
+// **** Logic to compare humanChoice and computerChoice and define the winner of the round
 
 function compareResults(cc, hc) {
+  let winnerMessage;
   if (cc == hc) {
     ties++;
-    return (winnerMessage = "This round ends in a tie.");
+    winnerMessage = "This round ends in a tie.";
   } else if (
     (cc == "rock" && hc == "scissors") ||
     (cc == "paper" && hc == "rock") ||
     (cc == "scissors" && hc == "paper")
   ) {
     computerScore++;
-    return (winnerMessage = `You lose! ${cc} beats ${hc}.`);
-  } else {
+    winnerMessage = `You lose! ${cc} beats ${hc}.`;
+  } else if (
+    (hc == "rock" && cc == "scissors") ||
+    (hc == "paper" && cc == "rock") ||
+    (hc == "scissors" && cc == "paper")
+  ) {
     humanScore++;
-    return (winnerMessage = `You win! ${hc} beats ${cc}.`);
+    winnerMessage = `You win! ${hc} beats ${cc}.`;
   }
+  displayMessage(winnerMessage);
 }
 
-function playRound() {
-  try {
-    log("Welcome to the Rock, Paper, Scissors simulator!");
-    getComputerChoice();
-    getHumanChoice();
-    log(compareResults(computerChoice, humanChoice));
-    log(
-      `The score is: Human = ${humanScore} | Computer = ${computerScore}. | Ties = ${ties}. `
-    );
-    log("");
-  } catch (error) {
-    log(error.message);
+// after playing a defined number of rounds, we decide who is the overall game winner.
+
+function decideWinner() {
+  const gameOverMessage = "GAME OVER.";
+  displayMessage(gameOverMessage);
+  let finalScore = `You won: ${humanScore} | Computer won: ${computerScore} | Ties: ${ties}.`;
+  displayMessage(finalScore);
+
+  let overallWinnerMessage;
+  if (computerScore == humanScore) {
+    overallWinnerMessage = "The overall game ends in a tie.";
+  } else if (humanScore > computerScore) {
+    overallWinnerMessage = "Congratulations! You won the overall game!";
+  } else {
+    overallWinnerMessage = "Sorry, you lost the overall game.";
   }
+  displayMessage(overallWinnerMessage);
+
+  addRestartButton();
+}
+
+function addRestartButton() {
+  const gameScreen = document.querySelector("#game-screen");
+  const restartButton = document.createElement("button");
+  restartButton.textContent = "Restart Game";
+  restartButton.addEventListener("click", function cleanGameScreen(e) {
+    let otherTarget = e.target.closest("button");
+    if (!otherTarget) return;
+    //cleans gameScreen after clicking the Restart button
+    gameScreen.innerHTML = "";
+    //resets variables
+    computerScore = 0;
+    humanScore = 0;
+    ties = 0;
+    gamesPlayed = 1;
+    //removes number of rounds
+    //returns functionality to the buttons (adds the event listener again)
+    playGame();
+    // removes the "number of rounds" text
+  });
+  gameScreen.appendChild(restartButton);
+  restartButton.focus();
 }
 
 function playGame() {
-  for (i = 1; i <= 5; i++) {
-    playRound();
-  }
-
-  if (computerScore == humanScore) {
-    log("The overall game ends in a tie.");
-  } else if (humanScore > computerScore) {
-    log("Congratulations! You won the overall game!");
-  } else {
-    log("Sorry, you lost the overall game.");
-  }
+  getHumanChoice(playRound);
 }
 
-playGame(computerScore, humanScore);
+playGame();
